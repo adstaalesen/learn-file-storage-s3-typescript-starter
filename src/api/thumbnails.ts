3600fda5a8541ae80273fe3e66d6b9dc9fdc64fa
adstaalesen <adrian.staalesen@icloud.com>
2026-01-06 20:35:20 +0100

    upload thumbnail

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,9 +1,9 @@
 import { getBearerToken, validateJWT } from "../auth";
 import { respondWithJSON } from "./json";
-import { getVideo } from "../db/videos";
+import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
-import { BadRequestError, NotFoundError } from "./errors";
+import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
 
 type Thumbnail = {
   data: ArrayBuffer;
@@ -47,7 +47,57 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
 
   console.log("uploading thumbnail for video", videoId, "by user", userID);
 
-  // TODO: implement the upload here
+  const formData = await req.formData();
 
-  return respondWithJSON(200, null);
+  const thumbnail = formData.get("thumbnail");
+
+  if (!(thumbnail instanceof File)) {
+    throw new BadRequestError("Invalid thumbnail file");
+  }
+
+  const maxUploadSize = 10 * 1024 * 1024; // 10MB
+
+  if (thumbnail.size > maxUploadSize) {
+    throw new BadRequestError("Thumbnail file is too large");
+  }
+
+  const buffer = await thumbnail.arrayBuffer();
+  const mediaType = thumbnail.type;
+
+  const {
+    title,
+    description,
+    videoURL,
+    createdAt,
+    updatedAt,
+    userID: videoUserID,
+  } = getVideo(cfg.db, videoId) ?? {};
+
+  if (!videoUserID || videoUserID !== userID) {
+    throw new UserForbiddenError(
+      "You are not authorized to upload a thumbnail for this video"
+    );
+  }
+
+  //  save thumbnail to global map
+  videoThumbnails.set(videoId, {
+    data: buffer,
+    mediaType,
+  });
+
+  const newThumbnailURL = `http://localhost:${cfg.port}/api/thumbnails/${videoId}`;
+
+  const newVideo = {
+    id: videoId,
+    createdAt: createdAt ?? new Date(),
+    updatedAt: updatedAt ?? new Date(),
+    title: title ?? "",
+    description: description ?? "",
+    thumbnailURL: newThumbnailURL,
+    userID,
+  };
+
+  updateVideo(cfg.db, newVideo);
+
+  return respondWithJSON(200, newVideo);
 }
