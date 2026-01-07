0d0959a4caf223a24a217959a5678ae0909d88b3
adstaalesen <adrian.staalesen@icloud.com>
2026-01-07 09:17:53 +0100

    encoding lesson

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -5,37 +5,6 @@ import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
 import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
 
-type Thumbnail = {
-  data: ArrayBuffer;
-  mediaType: string;
-};
-
-const videoThumbnails: Map<string, Thumbnail> = new Map();
-
-export async function handlerGetThumbnail(cfg: ApiConfig, req: BunRequest) {
-  const { videoId } = req.params as { videoId?: string };
-  if (!videoId) {
-    throw new BadRequestError("Invalid video ID");
-  }
-
-  const video = getVideo(cfg.db, videoId);
-  if (!video) {
-    throw new NotFoundError("Couldn't find video");
-  }
-
-  const thumbnail = videoThumbnails.get(videoId);
-  if (!thumbnail) {
-    throw new NotFoundError("Thumbnail not found");
-  }
-
-  return new Response(thumbnail.data, {
-    headers: {
-      "Content-Type": thumbnail.mediaType,
-      "Cache-Control": "no-store",
-    },
-  });
-}
-
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
   if (!videoId) {
@@ -45,59 +14,43 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const token = getBearerToken(req.headers);
   const userID = validateJWT(token, cfg.jwtSecret);
 
-  console.log("uploading thumbnail for video", videoId, "by user", userID);
+  const video = getVideo(cfg.db, videoId);
+  if (!video) {
+    throw new NotFoundError("Couldn't find video");
+  }
+  if (video.userID !== userID) {
+    throw new UserForbiddenError("Not authorized to update this video");
+  }
 
   const formData = await req.formData();
-
-  const thumbnail = formData.get("thumbnail");
-
-  if (!(thumbnail instanceof File)) {
-    throw new BadRequestError("Invalid thumbnail file");
+  const file = formData.get("thumbnail");
+  if (!(file instanceof File)) {
+    throw new BadRequestError("Thumbnail file missing");
   }
 
-  const maxUploadSize = 10 * 1024 * 1024; // 10MB
+  const MAX_UPLOAD_SIZE = 10 << 20;
 
-  if (thumbnail.size > maxUploadSize) {
-    throw new BadRequestError("Thumbnail file is too large");
-  }
-
-  const buffer = await thumbnail.arrayBuffer();
-  const mediaType = thumbnail.type;
-
-  const {
-    title,
-    description,
-    videoURL,
-    createdAt,
-    updatedAt,
-    userID: videoUserID,
-  } = getVideo(cfg.db, videoId) ?? {};
-
-  if (!videoUserID || videoUserID !== userID) {
-    throw new UserForbiddenError(
-      "You are not authorized to upload a thumbnail for this video"
+  if (file.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError(
+      `Thumbnail file exceeds the maximum allowed size of 10MB`
     );
   }
 
-  //  save thumbnail to global map
-  videoThumbnails.set(videoId, {
-    data: buffer,
-    mediaType,
-  });
+  const mediaType = file.type;
+  if (!mediaType) {
+    throw new BadRequestError("Missing Content-Type for thumbnail");
+  }
 
-  const newThumbnailURL = `http://localhost:${cfg.port}/api/thumbnails/${videoId}`;
+  const fileData = await file.arrayBuffer();
+  if (!fileData) {
+    throw new Error("Error reading file data");
+  }
 
-  const newVideo = {
-    id: videoId,
-    createdAt: createdAt ?? new Date(),
-    updatedAt: updatedAt ?? new Date(),
-    title: title ?? "",
-    description: description ?? "",
-    thumbnailURL: newThumbnailURL,
-    userID,
-  };
+  const base64Encoded = Buffer.from(fileData).toString("base64");
+  const base64DataURL = `data:${mediaType};base64,${base64Encoded}`;
 
-  updateVideo(cfg.db, newVideo);
+  video.thumbnailURL = base64DataURL;
+  updateVideo(cfg.db, video);
 
-  return respondWithJSON(200, newVideo);
+  return respondWithJSON(200, video);
 }
